31f83e54b63d3db8d7954cccbd030ca7c63fb0a4
Andrii Kozakevych <andriikozakevych@gmail.com>
2023-07-06 14:34:18 +0300

    create init files for event emitter tasks

---
+++ 3-event-emitter/WithTime.js
@@ -0,0 +1,10 @@
+class WithTime extends EventEmitter {
+  execute(asyncFunc, ...args) {}
+}
+
+const withTime = new WithTime();
+
+withTime.on('begin', () => console.log('About to execute'));
+withTime.on('end', () => console.log('Done with execute'));
+
+console.log(withTime.rawListeners("end"));
