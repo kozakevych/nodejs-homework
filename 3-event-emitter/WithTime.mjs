6205333ed367f9001f93f5180ca5e62a4dc2f866
Andrii Kozakevych <andriikozakevych@gmail.com>
2023-07-06 15:55:57 +0300

    task 2: add async request json method

--- 3-event-emitter/WithTime.mjs
+++ 3-event-emitter/WithTime.mjs
@@ -1,7 +1,10 @@
 import EventEmitter from './EventEmitter.mjs';
+import https from 'https';
 
 class WithTime extends EventEmitter {
-  execute(asyncFunc, ...args) {}
+  execute(asyncFunc, ...args) {
+    asyncFunc(this, ...args);
+  }
 }
 
 const withTime = new WithTime();
@@ -10,3 +13,27 @@ withTime.on('begin', () => console.log('About to execute'));
 withTime.on('end', () => console.log('Done with execute'));
 
 console.log(withTime.rawListeners("end"));
+
+const requestJson = (parent, url) => {
+  console.time('requestJson');
+  parent.emit('begin');
+
+  https.get(url, (response) => {
+    let data = '';
+
+    response.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    response.on('end', () => {
+      console.log(JSON.parse(data));
+      
+      parent.emit('end');
+      console.timeEnd('requestJson');
+    });
+  }).on('error', (error) => {
+    console.log(`Error: ${error.message}`);
+  });
+}
+
+withTime.execute(requestJson, 'https://jsonplaceholder.typicode.com/posts/1');
