4fee65189777484ab7420c41a83aeed1a40cdf7f
Andrii Kozakevych <andriikozakevych@gmail.com>
2023-08-28 17:36:06 +0300

    move req/res logic outside controller

--- 5-network/index.js
+++ 5-network/index.js
@@ -15,36 +15,119 @@ const router = new Router();
 
 router
   .get('/users', (req, res) => {
-    getAllUsers(req, res);
+    const result = getAllUsers(req, res);
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(result));
   })
   .post('/users', (req, res) => {
-    createUser(req, res);
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk;
+    });
+    req.on('end', () => {
+      const user = JSON.parse(body);
+      createUser(user);
+      res.statusCode = 201;
+      res.end('User created successfully');
+    });
   })
   .get('/users/:id', (req, res) => {
-    const userId = req.params.id;
-    getUserById(req, res, userId);
+    const result = getUserById(req.params.id);
+    if (!result) {
+      res.statusCode = 404;
+      res.end('User not found');
+      return;
+    }
+    res.setHeader('Content-Type', 'application/json');
+    res.statusCode = 200;
+    res.end(JSON.stringify(result));
   })
   .put('/users/:id', (req, res) => {
-    const userId = req.params.id;
-    updateUser(req, res, userId);
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk;
+    });
+    req.on('end', () => {
+      const { name, email } = JSON.parse(body);
+      if (!name && !email) {
+        res.statusCode = 400;
+        res.end('No data provided');
+        return;
+      }
+      const result = updateUser(req.params.id, name, email);
+      if (!result) {
+        res.statusCode = 404;
+        res.end('User not found');
+        return;
+      }
+      res.statusCode = 200;
+      res.end('User updated successfully');
+    });
   })
   .delete('/users/:id', (req, res) => {
-    const userId = req.params.id;
-    deleteUser(req, res, userId);
+    const result = deleteUser(req.params.id);
+    if (!result) {
+      res.statusCode = 404;
+      res.end('User not found');
+      return;
+    }
+    res.statusCode = 200;
+    res.end('User deleted successfully');
   })
   .get('/users/:id/hobbies', (req, res) => {
     res.setHeader('Cache-Control', 'max-age=3600');
     res.setHeader('Expires', new Date(Date.now() + 3600000).toUTCString());
-    const userId = req.params.id;
-    getUserHobbies(req, res, userId);
+    const result = getUserHobbies(req.params.id);
+    if (!result) {
+      res.statusCode = 404;
+      res.end('User not found');
+      return;
+    }
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(result));
   })
   .post('/users/:id/hobbies', (req, res) => {
-    const userId = req.params.id;
-    addHobbyToUser(req, res, userId);
+    let body = '';
+  
+    req.on('data', chunk => {
+      body += chunk;
+    });
+    req.on('end', () => {
+      const hobby = JSON.parse(body).hobby;
+      if (!hobby) {
+        res.statusCode = 400;
+        res.end('Hobby data not provided');
+        return;
+      }
+      const result = addHobbyToUser(req.params.id, hobby);
+      if (!result) {
+        res.statusCode = 404;
+        res.end('User not found');
+      }
+      res.statusCode = 200;
+      res.end('Hobby added successfully');
+    });
   })
   .delete('/users/:id/hobbies', (req, res) => {
-    const userId = req.params.id;
-    deleteHobbyFromUser(req, res, userId);
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk;
+    });
+    req.on('end', () => {
+      const hobby = JSON.parse(body).hobby;
+      if (!hobby) {
+        res.statusCode = 400;
+        res.end('Hobby data not provided');
+        return;
+      }
+      const result = deleteHobbyFromUser(req.params.id);
+      if (!result) {
+        res.statusCode = 404;
+        res.end('User not found');
+      }
+      res.statusCode = 200;
+      res.end('Hobby deleted successfully');
+    });
   });
 
 const server = http.createServer((req, res) => {
--- 5-network/userRoutes.js
+++ 5-network/userRoutes.js
@@ -1,30 +1,18 @@
 import { users } from './users-data.js';
 
-export const getAllUsers = (req, res) => {
+export const getAllUsers = () => {
   const usersList = users.map(u => {
     const { id, name, email } = u;
     return { id, name, email };
   });
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify(usersList));
+  return usersList;
 }
 
-export const createUser = (req, res) => {
-  let body = '';
-  req.on('data', chunk => {
-    body += chunk;
-  });
-  req.on('end', () => {
-    const user = JSON.parse(body);
+export const createUser = (user) => {
+  // Gets biggest 'id', increases its value by 1 and adds it to new user (just to make it unique)
+  // Should be handled with separate library, but not sure if that is needed in our task
+  const maxId = users.reduce((a, b) => a.id > b.id ? a : b).id;
+  user.id = maxId + 1;
 
-    // Gets biggest 'id', increases its value by 1 and adds it to new user (just to make it unique)
-    // Should be handled with separate library, but not sure if that is needed in our task
-    const maxId = users.reduce((a, b) => a.id > b.id ? a : b).id;
-    user.id = maxId + 1;
-
-    users.push(user);
-
-    res.statusCode = 201;
-    res.end('User created successfully');
-  });
+  users.push(user);
 }
