23597d6192d4323c1dcd8386572d3b4c7d872122
Andrii Kozakevych <andriikozakevych@gmail.com>
2023-08-30 18:20:13 +0300

    remove is deleted property from api response

--- 6-express/entities/cart.entity.ts
+++ 6-express/entities/cart.entity.ts
@@ -8,7 +8,7 @@ export interface CartItemEntity {
 export interface CartEntity {
   id: string; // uuid
   userId: string;
-  isDeleted: boolean;
+  isDeleted?: boolean;
   items: CartItemEntity[];
 }
 
@@ -20,6 +20,5 @@ const cartItem: CartItemEntity = {
 export const cart: CartEntity = {
   id: '1434fec6-cd85-420d-95c0-eee2301a971d',
   userId: '0fe36d16-49bc-4aab-a227-f84df899a6cb',
-  isDeleted: false,
   items: [cartItem],
 }
